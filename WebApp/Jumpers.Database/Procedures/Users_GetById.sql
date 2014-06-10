bbf9df9e95f2f78465052824fd8285e75059ad58
Dev-Duck <tom.haywood@easyroommate.com>
2014-06-10 23:56:41 +0100

    database project with profile for azure

---
+++ WebApp/Jumpers.Database/Procedures/Users_GetById.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[Users_GetById]
+	@id int
+AS
+	SELECT Id, Email
+	FROM [dbo].Users
+	WHERE Id = @id
+RETURN 0
---
+++ WebApp/Jumpers.Database/Tables/Users.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Email] NVARCHAR(100) NULL
+)
